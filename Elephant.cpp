f3e510e583c654c04d39f664a2a737078d226497
Samiya Akhter <samiyaaakhter10@gmail.com>
2026-02-20 22:37:55 +0600

    Add files via upload

---
+++ Elephant.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+#include<string>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a=n/5;;
+    if(n%5==0){
+        a+=0;}
+    else a+=1;
+    cout<<a;
+
+}
