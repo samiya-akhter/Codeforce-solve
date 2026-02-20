12729dbcabd85a1635b76a2bbc26a21224fbe65e
Samiya Akhter <samiyaaakhter10@gmail.com>
2026-02-20 22:36:18 +0600

    Add files via upload

---
+++ Soldier and Bananas.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int k,n,w;
+    cin>>k>>n>>w;
+    int sum=0;
+    for(int i=1; i<=w; i++){
+        sum+=k*i;
+    }
+    cout<<sum-n;
+}
