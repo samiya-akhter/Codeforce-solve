55934f75f4ff2016b53b4ca232f8e27fe525369b
Samiya Akhter <samiyaaakhter10@gmail.com>
2026-02-23 08:35:28 +0600

    Add files via upload

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m,n;
+    cin>>m>>n;
+
+    int a=(m*n)/2;
+    cout<<a;
+    
+    return 0;
+}
